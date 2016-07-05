540d6947594f7d264c12dcf2d968443a304892e0
naveen-fractlio <naveen.mh08@gmail.com>
2016-07-05 11:03:45 +0530

    Added upgrade directory and code update scripts for software update.

---
+++ install/upgrade/updater.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+echo "IntegralStor Upgrade started... Do not reboot or shutdown the system."
+
+echo "Pulling IntegralView updates"
+# Setup IntegralStor Common
+cd /tmp
+/bin/tar xzf integralstor_common.tar.gz
+yes | cp -rf /tmp/integralstor_common/site-packages/integralstor_common/* /opt/integralstor/integralstor_common/site-packages/integralstor_common
+yes | cp -rf /tmp/integralstor_common/scripts /opt/integralstor/integralstor_unicell
+
+rm -rf /tmp/integralstor_common
+
+# Setup IntegralStor UNICell
+cd /tmp
+/bin/tar xzf integralstor_unicell.tar.gz
+yes | cp -rf /tmp/integralstor_unicell/integral_view/* /opt/integralstor/integralstor_unicell/integral_view
+yes | cp -rf /tmp/integralstor_unicell/site-packages/integralstor_unicell/* /opt/integralstor/integralstor_unicell/site-packages/integralstor_unicell
+yes | cp -rf /tmp/integralstor_unicell/version /opt/integralstor/integralstor_unicell
+yes | cp -rf /tmp/integralstor_unicell/scripts /opt/integralstor/integralstor_unicell
+
+rm -rf /tmp/integralstor_unicell
+
+echo "IntegralView update successful"
+
+echo "Pulling Database updates"
+sqlite3 /opt/integralstor/integralstor_unicell/config/db/integral_view_config.db <<"EOF"
+ALTER TABLE email_config ADD COLUMN "email_audit" bool NOT NULL DEFAULT '0';
+EOF
+sqlite3 /opt/integralstor/integralstor_unicell/config/db/integral_view_config.db <<"EOF"
+ALTER TABLE email_config ADD COLUMN "email_quota" bool NOT NULL DEFAULT '0';
+EOF
+echo "Databases update successful."
+
+echo "IntegralStor UNICell has been successfully updated. System is Going to Restart now..."
+reboot
