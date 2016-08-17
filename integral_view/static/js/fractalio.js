da80d1ffb33fbdc0a372125be49f7da57bd2ce11
fractalram <ram@fractalio.com>
2016-08-17 10:33:19 +0530

    Changes to ease the display of required fields in forms

--- integral_view/static/js/fractalio.js
+++ integral_view/static/js/fractalio.js
@@ -1,21 +1,27 @@
-      function make_tab_active(id){
-        if (id === null) {
-        var url = location.pathname;
-        var a_url = ""
-        $(".nav-tabs li a").each(function(){
-          a_url = $(this).attr('href')
-          if (a_url === url){
-            $(this).parent('li').addClass('active')
-          }
-        });
-        }
-        else {
-          $("#"+id).addClass('active')
-        }
+function make_tab_active(id){
+  if (id === null) {
+    var url = location.pathname;
+    var a_url = "";
+    $(".nav-tabs li a").each(function(){
+      a_url = $(this).attr('href')
+      if (a_url === url){
+        $(this).parent('li').addClass('active')
       }
-      function make_side_menu_active(id){
-          $("#"+id).addClass('active')
-      };
-      $(".cover-page").click(function(){
-        $('.lightbox').show();
-      });
+    });
+  }
+  else {
+    $("#"+id).addClass('active')
+  }
+}
+
+function make_side_menu_active(id){
+  $("#"+id).addClass('active')
+};
+
+$(".cover-page").click(function(){
+  $('.lightbox').show();
+});
+
+$('.required').each(function() {
+  $(this).text("req");
+});
