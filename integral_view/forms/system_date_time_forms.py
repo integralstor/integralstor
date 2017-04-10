d8115a232ff99e33aa22e9728620a1e9ce039e6a
Ankit Sharma <sharma.ankit@email.com>
2017-04-10 13:04:33 +0530

    changes in datetime update

---
+++ integral_view/forms/system_date_time_forms.py
@@ -0,0 +1,17 @@
+from django import forms
+
+
+class DateTimeForm(forms.Form):
+    system_time = forms.CharField(required=False)
+    system_date = forms.CharField(required=False)
+
+    def clean(self):
+        cd = super(DateTimeForm, self).clean()
+        if 'system_time' not in cd:
+            print 'not in cd'
+        if ('system_time' not in cd or cd['system_time'] == '') and ('system_date' not in cd or cd['system_date'] == ''):
+            raise forms.ValidationError("Atleast date or time should be present")
+        else:
+            if cd['system_date'] == '': cd['system_date'] = None
+            if cd['system_time'] == '': cd['system_time'] = None
+            return cd 
--- integral_view/views/system.py
+++ integral_view/views/system.py
@@ -1,53 +1,55 @@
 import django
 import django.template
 
-from integral_view.forms import system_date_time_form
-from integralstor_utils.system_date_time import update_date_time
+from integral_view.forms import system_date_time_forms
+from integralstor_utils import date_time 
 
 def input_date_time(request):
     return_dict = {}
     try:
-        form = system_date_time_form.date_time_form()
+        form = system_date_time_forms.DateTimeForm()
         return_dict['form'] = form
     except Exception, e:
-        return_dict['base_template'] = "view_date_time.html"
+        return_dict['base_template'] = "update_system_date_time.html"
         return_dict["page_title"] = 'Update system and hardware date and time'
         return_dict["error"] = 'Error viewing date time'
         return_dict["error_details"] = str(e)
         return django.shortcuts.render_to_response("logged_in_error.html", return_dict, context_instance=django.template.context.RequestContext(request))
     else:
-        return django.shortcuts.render_to_response("view_date_time.html", return_dict, context_instance=django.template.context.RequestContext(request))
+        return django.shortcuts.render_to_response("update_system_date_time.html", return_dict, context_instance=django.template.context.RequestContext(request))
 
 def update_system_date_time(request):
-    return_dict = {}
-    try:
+        return_dict = {}
+#    try:
         if request.method == 'POST':
-            form = system_date_time_form.date_time_form(request.POST)
+            form = system_date_time_forms.DateTimeForm(request.POST)
             if form.is_valid():
                 cd = form.cleaned_data
-                output, err = update_date_time(cd["system_date"], cd["system_time"])
+                output, err = date_time.update_date_time(cd["system_date"], cd["system_time"])
                 if err:
                     raise Exception(err)
                 else:
                     if "date_set" in output and "time_set" in output:
                         print "datetime"
                         if output["date_set"] == True and output["time_set"] == True:
-                            url = "/view_system_info?ack=datetime"
+                            url = "/view_system_info?ack=system_datetime_set"
                     elif "time_set" in output:
                         print "time"
                         if output["time_set"] == True:
-                            url="/view_system_info?ack=time"
+                            url="/view_system_info?ack=system_time_set"
                     elif "date_set" in output:
                         print "date"
                         if output["date_set"] == True:
-                            url="/view_system_info?ack=date"
+                            url="/view_system_info?ack=system_date_set"
                     return django.http.HttpResponseRedirect(url)
             else:
                 return_dict["form"] = form
-                return django.shortcuts.render_to_response("view_date_time.html", return_dict, context_instance=django.template.context.RequestContext(request))
-    except Exception, e:
-        return_dict["error"] = 'Error Performing Date Time Update'
-        return_dict["error_details"] = str(e)
-        return django.shortcuts.render_to_response("logged_in_error.html",  context_instance=django.template.context.RequestContext(request))
-    else:
-        return django.http.HttpResponseRedirect("/view_system_info")
+                return django.shortcuts.render_to_response("update_system_date_time.html", return_dict, context_instance=django.template.context.RequestContext(request))
+#    except Exception, e:
+#        return_dict["base_template"] = 'update_system_date_time.html'
+#        return_dict['page_title'] = 'Update system and hardware date and time'
+#        return_dict["error"] = 'Error Performing Date Time Update'
+#        return_dict["error_details"] = str(e)
+#        return django.shortcuts.render_to_response("logged_in_error.html", return_dict, context_instance=django.template.context.RequestContext(request))
+#    else:
+#        return django.http.HttpResponseRedirect("/view_system_info")
