1aa3e8000e7b676afb3fcadf6627991cc84c62e6
six-k <ramsri.hp@gmail.com>
2017-09-13 12:01:55 +0530

    Handle quotations in install script
    Signed-off-by: six-k <ramsri.hp@gmail.com>

--- install/configure_integralstor.sh
+++ install/configure_integralstor.sh
@@ -3,5 +3,5 @@
 hardware_vendor=""
 [ -n "$1" ] && hardware_vendor=$1
 
-/bin/bash -c '/opt/integralstor/integralstor/install/scripts/configure_os.sh $hardware_vendor'
-/bin/bash -c '/opt/integralstor/integralstor/install/scripts/configure_services.sh'
+echo "/opt/integralstor/integralstor/install/scripts/configure_os.sh $hardware_vendor" | /bin/bash
+echo "/opt/integralstor/integralstor/install/scripts/configure_services.sh" | /bin/bash
