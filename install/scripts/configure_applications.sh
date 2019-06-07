b8b1e76af794d118a59740b3f68ac5caec57fcd7
fractalram <ram@fractalio.com>
2019-06-07 10:29:21 +0530

    Delete configure_applications.sh

--- install/scripts/configure_applications.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-ISTOR_ROOT="/opt/integralstor/integralstor"
-
-if [[ ! -e "$ISTOR_ROOT/config/conf_files/applications.json" ]];then                        
-        cp $ISTOR_ROOT/install/conf-files/others/applications.json $ISTOR_ROOT/config/conf_files/
-else
-        echo "$ISTOR_ROOT/config/conf_files/applications.json already exists not overwriting it.." 
-fi      
