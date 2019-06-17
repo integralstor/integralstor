77febec496dd1e7a60b90a0b86cc0788a34fb398
Purushotham K S <purushotham.0608@gmail.com>
2019-06-17 18:08:51 +0530

    Modified get_enclosure_devices() to use lsscsi instead of sg_map

--- scripts/python/check_jbod_psu_status.py
+++ scripts/python/check_jbod_psu_status.py
@@ -28,7 +28,7 @@ def main():
                     product = re.findall(r'(^Product: *)(\w+)', line)
                     if product:
                         product_info = product[0][1]
-            alert_str = '%s has gone down on %s %s' % (faulty_psu, vendor_info, product_info)
+            alert_str = 'PSU - %s has gone down on %s %s' % (faulty_psu, vendor_info, product_info)
             alert_list.append({'subsystem_type_id': 5, 'severity_type_id': 3,
                                 'component': 'Power Supply', 'alert_str': alert_str})
             ret, err = alerts.record_alerts(alert_list)
@@ -36,6 +36,8 @@ def main():
                 raise Exception(err)
     except Exception, e:
         return None, "Error recording PSU alert : %s" % str(e)
+    else:
+	return True, None
 
 if __name__ == "__main__":
     ret = main()
