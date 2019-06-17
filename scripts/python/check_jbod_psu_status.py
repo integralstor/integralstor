b3b8d58ae64e813db7e4f04ce7954201aad58e9e
Purushotham K S <purushotham.0608@gmail.com>
2019-06-17 16:42:39 +0530

    Created separate file for sg_util functions, modified get_psu_status() to return a list

--- scripts/python/check_jbod_psu_status.py
+++ scripts/python/check_jbod_psu_status.py
@@ -1,85 +1,39 @@
-from integralstor import command, alerts
+from integralstor import command, alerts, disk_enclosures
 import re, sys
-
-def get_enclosure_devices():
-    # Returns a list of /dev/sgX path for Connected Storage Enclosures
-    enclosures = []
-    try:
-        sg_devs, err = command.get_command_output('sg_map -i')
-        if err:
-            raise Exception(err)
-
-        if sg_devs:
-            for sg_dev in sg_devs:
-                    encl = re.findall('ENCL', sg_dev, re.IGNORECASE)
-                    if encl:
-                        res = re.findall(r'^\/\w+\/\w+', sg_dev)
-                        if res:
-                            enclosures += res
-                    else:
-                        raise Exception("No enclosures found")
-    except Exception, e:
-        return None, "Error retrieving enclosures : %s" % str(e)
-    else:
-        return enclosures, None
-
-def get_psu_status():
-    psu_status = {}
-    try:
-        encls, err = get_enclosure_devices()
-        if err:
-            raise Exception(err)
-
-        if encls:
-            for encl in encls:
-                for psu in range(2):
-                    tmp_cmd_output, err = command.get_command_output('sg_ses --index=ps,%d %s' % (psu, encl))
-                    if err:
-                        raise Exception(err)
-                    if tmp_cmd_output:
-                        for status_line in tmp_cmd_output:
-                            result = re.findall(r'status: \w+', status_line)
-                            if result:
-                                psu_status['PSU-%d' % psu] = result[0]
-    except Exception, e:
-        return None, "Error retrieving JBOD PSU status : %s" % str(e)
-    else:
-        return psu_status, None
 
 def main():
     try:
         vendor_info = 'N.A'
         product_info = 'N.A'
-        psu_status, err = get_psu_status()
+        alert_list = []
+        psu_status, err = disk_enclosures.get_psu_status()
         if err:
             raise Exception(err)
 
         if psu_status:
-            if 'status: Critical' in psu_status.values():
-                alert_list = []
-                for psu in psu_status:
-                    if psu_status[psu] == 'status: Critical':
-                        faulty_psu = psu
-                encls, err = get_enclosure_devices()
+            for psu_num, status_line in enumerate(psu_status):
+                if 'status: Critical' in status_line:
+                    faulty_psu = psu_num
+            encls, err = disk_enclosures.get_enclosure_devices()
+            if err:
+                raise Exception(err)
+            for encl in encls:
+                tmp_cmd_output, err = command.get_command_output('sginfo %s' % encl)
                 if err:
                     raise Exception(err)
-                for encl in encls:
-                    tmp_cmd_output, err = command.get_command_output('sginfo %s' % encl)
-                    if err:
-                        raise Exception(err)
-                    for line in tmp_cmd_output:
-                        vendor = re.findall(r'(^Vendor: *)(\w+)', line)
-                        if vendor:
-                            vendor_info = vendor[0][1]
-                        product = re.findall(r'(^Product: *)(\w+)', line)
-                        if product:
-                            product_info = product[0][1]
-                alert_str = '%s has gone down on %s %s' % (faulty_psu, vendor_info, product_info)
-                alert_list.append({'subsystem_type_id': 5, 'severity_type_id': 3,
-                                    'component': 'Power Supply', 'alert_str': alert_str})
-                ret, err = alerts.record_alerts(alert_list)
-                if err:
-                    raise Exception(err)
+                for line in tmp_cmd_output:
+                    vendor = re.findall(r'(^Vendor: *)(\w+)', line)
+                    if vendor:
+                        vendor_info = vendor[0][1]
+                    product = re.findall(r'(^Product: *)(\w+)', line)
+                    if product:
+                        product_info = product[0][1]
+            alert_str = '%s has gone down on %s %s' % (faulty_psu, vendor_info, product_info)
+            alert_list.append({'subsystem_type_id': 5, 'severity_type_id': 3,
+                                'component': 'Power Supply', 'alert_str': alert_str})
+            ret, err = alerts.record_alerts(alert_list)
+            if err:
+                raise Exception(err)
     except Exception, e:
         return None, "Error recording PSU alert : %s" % str(e)
 
